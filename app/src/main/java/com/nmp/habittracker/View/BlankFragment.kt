25a1d991e5aad2508205d29cb185412f7dc6ba31
JOSEPH XELA <joseph.xela1234@gmail.com>
2026-05-02 15:25:14 +0700

    update blank fragment

--- app/src/main/java/com/nmp/habittracker/View/BlankFragment.kt
+++ app/src/main/java/com/nmp/habittracker/View/BlankFragment.kt
@@ -5,56 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nmp.habittracker.R
+import com.nmp.habittracker.ViewModel.ListViewModel
+import com.nmp.habittracker.databinding.FragmentBlankBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BlankFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentBlankBinding
+    private lateinit var viewModel: ListViewModel
+    private val blankListAdapter = BlankListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        binding = FragmentBlankBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        binding.recViewHabit.layoutManager = LinearLayoutManager(context)
+        binding.recViewHabit.adapter = blankListAdapter
+
+        observeViewModel()
+
+        binding.refreshLayout.setOnRefreshListener {
+            binding.recViewHabit.visibility = View.GONE
+            binding.txtError.visibility = View.GONE
+            binding.progressLoad.visibility = View.VISIBLE
+            viewModel.refresh()
+            binding.refreshLayout.isRefreshing = false
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    fun observeViewModel(){
+        viewModel.habitsLD.observe(viewLifecycleOwner, Observer {
+            blankListAdapter.updateHabitList(it)
+        })
+
+        viewModel.habitLoadErrorLD.observe(viewLifecycleOwner, Observer{
+            if (it==true){
+                binding.txtError?.visibility = View.VISIBLE
+            }else{
+                binding.txtError?.visibility = View.GONE
             }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer{
+            if (it==true){
+                binding.txtError?.visibility = View.GONE
+                binding.progressLoad.visibility = View.VISIBLE
+            }else{
+                binding.txtError?.visibility = View.VISIBLE
+                binding.progressLoad.visibility = View.GONE
+            }
+        })
     }
 }
