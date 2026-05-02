2bfd342d508fdc68febbdde0b8bc1e12efefb902
Arron060502 <83685781+Arron060502@users.noreply.github.com>
2026-05-02 18:03:25 +0700

    modified:   app/src/main/java/com/nmp/habittracker/View/LoginFragment.kt

--- app/src/main/java/com/nmp/habittracker/View/LoginFragment.kt
+++ app/src/main/java/com/nmp/habittracker/View/LoginFragment.kt
@@ -1,37 +1,25 @@
 package com.nmp.habittracker.View
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import com.nmp.habittracker.R
 import com.nmp.habittracker.ViewModel.LoginViewModel
 import com.nmp.habittracker.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
     private lateinit var viewModel: LoginViewModel
     private lateinit var binding: FragmentLoginBinding
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -44,31 +32,25 @@ class LoginFragment : Fragment() {
         observeViewModel()
 
         binding.btnLogin.setOnClickListener {
-             val username = binding.txtUsername.text.toString()
-    val password = binding.txtPassword.text.toString()
+            val username = binding.txtUsername.text.toString()
+            val password = binding.txtPassword.text.toString()
 
-    if (username == "student" && password == "123") {
-        Toast.makeText(requireContext(), "Login berhasil", Toast.LENGTH_SHORT).show()
-
-        // pindah ke dashboard
-        val action = LoginFragmentDirections.actionLoginToDashboard()
-        Navigation.findNavController(it).navigate(action)
-
-    } else {
-        Toast.makeText(requireContext(), "Username / Password salah", Toast.LENGTH_SHORT).show()
-    }
+            viewModel.login(username, password)
+        }
     }
 
-    fun observeViewModel() {
+    private fun observeViewModel() {
         viewModel.loginSuccessLD.observe(viewLifecycleOwner, Observer {
             if (it == true) {
-                val action = LoginFragmentDirections.actionBlankFragment()
+                Toast.makeText(requireContext(), "Login berhasil", Toast.LENGTH_SHORT).show()
+
+                val action = LoginFragmentDirections.actionLoginToDashboard()
                 Navigation.findNavController(requireView()).navigate(action)
             }
         })
 
         viewModel.errorLD.observe(viewLifecycleOwner, Observer {
-            if (it != null && viewModel.loginSuccessLD.value == false) {
+            it?.let {
                 Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
             }
         })
